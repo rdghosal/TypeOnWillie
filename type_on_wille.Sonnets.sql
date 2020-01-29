6d9b4a2608ffc3e49ebba9337ac7b60ff45ef472
Rahul D. Ghosal <rdghosal@gmail.com>
2020-01-29 09:31:04 -0500

    changed dir struct

--- type_on_wille.Sonnets.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [type_on_willie].[sonnets]
-(
-	[sonnet_id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
-    [sonnet_number] INT NOT NULL,
-    [sonnet_title] VARCHAR(255) NOT NULL, 
-    [sonnet_text] VARCHAR(255) NOT NULL
-)
