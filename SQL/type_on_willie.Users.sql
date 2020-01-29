cc78aec71f8912c55893f920c15b98c0c0669f38
Rahul D. Ghosal <rdghosal@gmail.com>
2020-01-29 09:30:06 -0500

    changed dir struct

---
+++ SQL/type_on_willie.Users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [type_on_willie].[Sonnets] (
+    [user_id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    [name] VARCHAR(255) NOT NULL,
+    [age] INT NULL,
+    [location] VARCHAR(255) NULL
+)
