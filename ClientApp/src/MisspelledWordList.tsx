9aa81aff84d3ae25ab81ca2946dd93050ea813c2
Rahul D. Ghosal <rdghosal@gmail.com>
2020-03-04 22:30:32 -0500

    Partial bug fix for list

--- ClientApp/src/MisspelledWordList.tsx
+++ ClientApp/src/MisspelledWordList.tsx
@@ -14,12 +14,28 @@ const MisspelledWordList = ({ misspelledWords, lineIndex } : MisspelledWordListP
         // Cache misspelled words as list for mapping to li els
         const temp = misspelledList;
         const currLineList = misspelledWords[lineIndex];
-        let newList = (temp && currLineList) ? [...temp, currLineList[currLineList.length - 1] ] : misspelledWords[lineIndex];
+
+        // TODO get up to last word in line
+
+        let newList; 
+        if (!temp && currLineList) {
+            // Init misspelledList
+            newList = [ currLineList[currLineList.length - 1] ]; 
+        } else if (temp && currLineList) {
+            // Preserve previous and concatenate
+            newList = [ ...temp, currLineList[currLineList.length - 1]];
+        } else if (temp && !currLineList) {
+            // New line
+            newList = temp;
+        }
+
+        if (!newList) return;
         concatMisspelled(newList);
     }, [lineIndex, misspelledWords]);
 
     return (
         <div>
+            { console.log(misspelledWords) }
             <div className="typesession__incorrect-list">
                 <ul>
                     {
