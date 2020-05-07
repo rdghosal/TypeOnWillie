39fbe787cbff1d987714f61c7093705ec528c63b
rdghosal <rdghosal@gmail.com>
2020-05-07 11:42:02 -0400

    Added sql queries

--- DataAccess/TypeSessionCommand.cs
+++ DataAccess/TypeSessionCommand.cs
@@ -20,5 +20,41 @@ namespace TypeOnWillie.DataAccess
         public const string SELECT_ALL = @"SELECT [SecondsElapsed], [PercentCorrect], [MisspelledWords] 
                                            FROM [type_on_willie].[dbo].[TypeSessions]
                                            WHERE [UserId] = @userId;";
+        public const string SELECT_SCORES_LASTYEAR = @"SELECT 
+                                                        MONTH([DateTime]) AS 'Month', 
+                                                        AVG([TypedWordCount]/[SecondsElapsed] * 60) AS 'AverageWpm',
+                                                        AVG([CorrectWordCount]*1.0 / [TypedWordCount]*1.0) AS 'AverageAccuracy'
+                                                     FROM [type_on_willie].[dbo].[TypeSessions] ts
+                                                     WHERE ts.UserId = @userId,
+                                                        AND ts.Quit = 'N',
+                                                        AND ts.DateTime BETWEEN GETDATE() AND DATEADD(year, -1, GETDATE())
+                                                     GROUP BY MONTH([DateTime]);";
+        public const string SELECT_SCORES_BYMONTH = @"SELECT
+                                                        DAY([DateTime]) AS 'Day',
+                                                        AVG([TypedWordCount]/[SecondsElapsed] * 60) AS 'AverageWpm',
+                                                        AVG([CorrectWordCount]*1.0 / [TypedWordCount]*1.0) AS 'AverageAccuracy'
+                                                     FROM [type_on_willie].[dbo].[TypeSessions] ts
+                                                     WHERE ts.UserId = @userId,
+                                                        AND ts.Quit = 'N',
+                                                        AND MONTH(ts.DateTime) = @month
+                                                     GROUP BY DAY([DateTime]);";
+        public const string SELECT_TOP_SCORES = @"SELECT
+                                                    (SELECT 
+                                                        [SonnetId]
+                                                     FROM [type_on_willie].[dbo].[TypeSessions] ts,
+                                                     WHERE ts.UserId = @userId
+                                                     GROUP BY [SonnetId]
+                                                     HAVING MAX(COUNT([SonnetId])) AS 'TopSonnet',
+                                                    (SELECT 
+                                                        MAX([TypedWordCount]/[SecondsElapsed] * 60)
+                                                     FROM [type_on_willie].[dbo].[TypeSessions] ts
+                                                     WHERE ts.UserId = @userId
+                                                        AND ts.Quit = 'N') AS 'TopWpm',
+                                                    (SELECT
+                                                        MAX([CorrectWordCount]*1.0 / [TypedWordCount]*1.0)
+                                                    FROM [type_on_willie].[dbo].[TypeSessions] ts
+                                                    WHERE ts.UserId = @userId
+                                                        AND ts.Quit = 'N') AS 'TopAccuracy'
+                                                FROM [type_on_willie].[dbo].[TypeSessions];";
     }
 }
