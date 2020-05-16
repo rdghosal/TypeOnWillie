eb5cb97cadfc986c13da6f36f8158c7d01574c8a
rdghosal <rdghosal@gmail.com>
2020-05-16 13:10:11 -0400

    Split Profile model between User/Sys

---
+++ Models/ProfileParamsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TypeOnWillie.Models
+{
+    public class ProfileParamsDto
+    {
+        public string UserId { get; set; }
+
+        public int Month { get; set; }
+
+        public int Year { get; set; }
+
+        public string CurrentDate { get; set; } = null;
+    }
+}
---
+++ Models/UserProfileDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TypeOnWillie.Models
+{
+    public class UserProfileDto : Profile
+    {
+        public User User { get; set; }
+    }
+}
