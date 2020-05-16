4fd6fae343f37c1dc6313afa477988b7568b525d
rdghosal <rdghosal@gmail.com>
2020-05-16 13:10:11 -0400

    Split Profile model between User/Sys

---
+++ Models/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TypeOnWillie.Models
+{
+    public class Profile
+    {
+        public UserMetrics Metrics { get; set; }
+        public RecordCollection Records { get; set; }
+        public IEnumerable<ScoreCollection> Scores { get; set; }
+        public IEnumerable<string> TopMisspellings { get; set; }
+    }
+}
---
+++ Models/SysProfileDto.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis.Profiling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TypeOnWillie.Models
+{
+    public class SysProfileDto : Profile
+    {
+    }
+}
