afd93041361a1bc0db03b6c027d565b96b201585
Rahul Ghosal <rdghosal@gmail.com>
2021-02-11 23:40:51 -0500

    Made entrypoint script more responsive

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
-echo "Executing backup of ${DBNAME}"
-/opt/mssql/bin/sqlservr --accept-eula & sleep 60 \
+echo "Executing backup of ${DBNAME}."
+/opt/mssql/bin/sqlservr --accept-eula > /sqlservr.out & tail -f /sqlservr.out | grep -qe "Serv.*start" && sleep 5 \
 	&& /opt/mssql-tools/bin/sqlcmd -S localhost -U $DB_USER -P $SA_PASSWORD -Q "RESTORE DATABASE ${DBNAME} FROM DISK = N'/${DBNAME}.bak' WITH REPLACE, MOVE '${DBNAME}' TO '/var/opt/mssql/data/${DBNAME}.mdf', MOVE '${DBNAME}_log' TO '/var/opt/mssql/data/${DBNAME}.ldf'" \
 	&& pkill sqlservr
 echo "Backup complete."
